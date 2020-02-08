4e772bf8a8da2d6870eac9de88635c1e3c3e522a
databunny <yi@databunny.sg>
2020-02-09 00:25:43 +0800

    Add collect IP task

---
+++ collectIP.R
@@ -0,0 +1,24 @@
+library(jsonlite)
+library(RCurl)
+library(dplyr)
+DEV<-FALSE
+PROD<-TRUE
+collectLocalWMI<-function(PSCommand)
+{
+  cmd<-paste0("powershell -command \"get-wmiobject ", PSCommand)
+  cat(cmd)
+  command<-system(cmd,intern=TRUE)
+  return(fromJSON(command))
+}
+
+result<-collectLocalWMI("Win32_NetworkAdapterConfiguration |select macaddress,ipaddress,Description |ConvertTo-Json \"")
+result[is.na(result)]<-""
+result[result=="NULL"]<-""
+result<-result%>%filter(ipaddress!="")
+
+payloadcontent<-URLencode(base64_enc(serialize(result,NULL)))
+if(DEV) getForm("http://127.0.0.1:4738/upload",table="computer",mergekey="macaddress",payload=payloadcontent)
+if(PROD) getForm("http://192.168.1.150:1500/upload",table="computer",mergekey="macaddress",payload=payloadcontent)
+
+
+
