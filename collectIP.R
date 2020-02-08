4c8178dc1d2065163c1399d9b999c6a02377243b
databunny <yi@databunny.sg>
2020-02-09 00:55:37 +0800

    add while true loop and sleep 3 second

--- collectIP.R
+++ collectIP.R
@@ -10,15 +10,21 @@ collectLocalWMI<-function(PSCommand)
   command<-system(cmd,intern=TRUE)
   return(fromJSON(command))
 }
-
-result<-collectLocalWMI("Win32_NetworkAdapterConfiguration |select macaddress,ipaddress,Description |ConvertTo-Json \"")
-result[is.na(result)]<-""
-result[result=="NULL"]<-""
-result<-result%>%filter(ipaddress!="")
-
-payloadcontent<-URLencode(base64_enc(serialize(result,NULL)))
-if(DEV) getForm("http://127.0.0.1:4738/upload",table="computer",mergekey="macaddress",payload=payloadcontent)
-if(PROD) getForm("http://192.168.1.150:1500/upload",table="computer",mergekey="macaddress",payload=payloadcontent)
-
+while(TRUE)
+{
+  try(
+    {
+      result<-collectLocalWMI("Win32_NetworkAdapterConfiguration |select macaddress,ipaddress,Description |ConvertTo-Json \"")
+      result[is.na(result)]<-""
+      result[result=="NULL"]<-""
+      result<-result%>%filter(ipaddress!="")
+      
+      payloadcontent<-URLencode(base64_enc(serialize(result,NULL)))
+      if(DEV) getForm("http://127.0.0.1:4738/upload",table="computer",mergekey="macaddress",payload=payloadcontent)
+      if(PROD) getForm("http://192.168.1.150:1500/upload",table="computer",mergekey="macaddress",payload=payloadcontent)
+      Sys.sleep(3000)
+    }
+  )
+}
 
 
